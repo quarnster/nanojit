efa25d45dbd92a500897c4fff138aeebcd86bae3
Fredrik Ehnbom <quarnster@gmail.com>
2012-07-16 19:31:49 +0200

    Progress at making it compile.

--- hack/avmplus.h
+++ hack/avmplus.h
@@ -4,28 +4,79 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <memory.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 namespace avmplus
 {
+    static inline void AvmLog(const char * fmt, ...)
+    {
+        va_list va;
+        va_start(va, fmt);
+        vprintf(fmt, va);
+        va_end(va);
+    }
 
 }
 
-typedef struct
+struct float4_t
 {
-    union
-    {
-        float f[4];
-        struct { float x; float y; float z; float w; };
-    };
-} float4_t;
 
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+static inline float4_t operator+(const float4_t& self, const float4_t& other)
+{
+    float4_t ret = {self.x+other.x, self.y+other.y, self.z+other.z, self.w+other.w};
+    return ret;
+}
+static inline float4_t operator-(const float4_t& self, const float4_t& other)
+{
+    float4_t ret = {self.x-other.x, self.y-other.y, self.z-other.z, self.w-other.w};
+    return ret;
+}
+static inline float4_t operator*(const float4_t& self, const float4_t& other)
+{
+    float4_t ret = {self.x*other.x, self.y*other.y, self.z*other.z, self.w*other.w};
+    return ret;
+}
+static inline float4_t operator/(const float4_t& self, const float4_t& other)
+{
+    float4_t ret = {self.x/other.x, self.y/other.y, self.z/other.z, self.w/other.w};
+    return ret;
+}
+
+static inline bool operator==(const float4_t& self, const float4_t& other)
+{
+    return self.x == other.x && self.y == other.y && self.z == other.z && self.w == other.w;
+}
 
 #define f4_x(f) (f).x
 #define f4_y(f) (f).y
 #define f4_z(f) (f).z
 #define f4_w(f) (f).w
-#define VMPI_memset memset
+#define f4_add(a, b) (a) + (b)
+#define f4_sub(a, b) (a) - (b)
+#define f4_mul(a, b) (a) * (b)
+#define f4_div(a, b) (a) / (b)
+#define f4_eq_i(a, b) (a) == (b)
 
+#define VMPI_memset memset
+#define VMPI_abort abort
+#define VMPI_memcmp memcmp
+#define VMPI_strcpy strcpy
+#define VMPI_strlen strlen
+
+#define PERFM_NVPROF(a, b)
+
+static inline int VMPI_getVMPageSize()
+{
+    return 0;
+}
 
 #endif
 
